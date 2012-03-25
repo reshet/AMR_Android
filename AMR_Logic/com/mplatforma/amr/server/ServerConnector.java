f987c590dfb7c866c8fd56d88a038ddd72dfe679
reshet <reshet.ukr@gmail.com>
2012-03-26 02:45:40 +0300

    last ICE 3 with play video!!!+ZOOM!!!

--- AMR_Logic/com/mplatforma/amr/server/ServerConnector.java
+++ AMR_Logic/com/mplatforma/amr/server/ServerConnector.java
@@ -202,4 +202,62 @@ public class ServerConnector {
 		}
     	String response="";
     }
+	
+	
+	 private class ProgressLoadPDFThread extends Thread {	
+         
+         // Class constants defining state of the thread
+         final static int DONE = 0;
+         final static int RUNNING = 1;
+         
+         Handler mHandler;
+         int mState;
+         int total;
+     
+         // Constructor with an argument that specifies Handler on main thread
+         // to which messages will be sent by this thread.
+         
+         ProgressLoadPDFThread(Handler h) {
+             mHandler = h;
+         }
+         
+         // Override the run() method that will be invoked automatically when 
+         // the Thread starts.  Do the work required to update the progress bar on this
+         // thread but send a message to the Handler on the main UI thread to actually
+         // change the visual representation of the progress. In this example we count
+         // the index total down to zero, so the horizontal progress bar will start full and
+         // count down.
+         
+         @Override
+         public void run() {
+             mState = RUNNING;   
+            // total = maxBarValue;
+             while (mState == RUNNING) {
+                 // The method Thread.sleep throws an InterruptedException if Thread.interrupt() 
+                 // were to be issued while thread is sleeping; the exception must be caught.
+                 try {
+                     // Control speed of update (but precision of delay not guaranteed)
+                     Thread.sleep(1000);
+                 } catch (InterruptedException e) {
+                     Log.e("ERROR", "Thread was Interrupted");
+                 }
+                 
+                 // Send message (with current value of  total as data) to Handler on UI thread
+                 // so that it can update the progress bar.
+                 
+                 Message msg = mHandler.obtainMessage();
+                 Bundle b = new Bundle();
+                 b.putInt("total", total);
+                 msg.setData(b);
+                 mHandler.sendMessage(msg);
+                 
+                 total--;    // Count down
+             }
+         }
+         
+         // Set current state of thread (use state=ProgressThread.DONE to stop thread)
+         public void setState(int state) {
+             mState = state;
+         }
+     }
 }
--- PDFViewer/net/sf/andpdf/pdfviewer/PdfViewerICE2Activity.java
+++ PDFViewer/net/sf/andpdf/pdfviewer/PdfViewerICE2Activity.java
@@ -33,23 +33,30 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.PointF;
 import android.graphics.RectF;
 import android.graphics.Bitmap.Config;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.util.FloatMath;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -75,7 +82,7 @@ import db.DataHelper;
  * U:\Android\android-sdk-windows-1.5_r1\tools\adb push u:\Android\simple_T.pdf /data/test.pdf
  * @author ferenc.hechler
  */
-public class PdfViewerICE2Activity extends Activity {
+public class PdfViewerICE2Activity extends Activity{
 
 	private DataHelper dhlpr;
 	
@@ -96,6 +103,8 @@ public class PdfViewerICE2Activity extends Activity {
 	
 	private final static int DIALOG_PAGENUM = 1;
 	private final static int DIALOG_LOAD = 2;
+	private final static int DIALOG_UNZIPPING = 3;
+	
 	
 	private GraphView mOldGraphView;
 	private GraphView mGraphView;
@@ -428,11 +437,19 @@ public class PdfViewerICE2Activity extends Activity {
 	        progressDialog.setMessage("Loading...");
 	        progressDialog.setCancelable(false);
 			return progressDialog;
+        case DIALOG_UNZIPPING:
+	        ProgressDialog progressDialog2;
+	        progressDialog2 = new ProgressDialog(this);
+	        progressDialog2.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+	        progressDialog2.setMessage("Loading...");
+	        progressDialog2.setCancelable(false);
+			return progressDialog2;
+        	
         }        
         return null;
     }
     
-	private class GraphView extends FullScrollView {
+	private class GraphView extends FullScrollView implements OnTouchListener{
     	private String mText;
     	private long fileMillis;
     	private long pageParseMillis;
@@ -452,6 +469,130 @@ public class PdfViewerICE2Activity extends Activity {
         {
         	//bar.setVisibility(ProgressBar.INVISIBLE);
         }
+        Matrix matrix = new Matrix();
+        Matrix savedMatrix = new Matrix();
+        PointF start = new PointF();
+        PointF mid = new PointF();
+        Float oldDist = (float) 0;
+
+        // We can be in one of these 3 states
+        static final int NONE = 0;
+        static final int DRAG = 1;
+        static final int ZOOM = 2;
+        int mode = NONE;
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+           CoolLayout vie = (CoolLayout) v;
+           ImageView view = vie.getImageView();
+           // Dump touch event to log
+           dumpEvent(event);
+
+           // Handle touch events here...
+           switch (event.getAction() & MotionEvent.ACTION_MASK) {
+           case MotionEvent.ACTION_DOWN:
+              savedMatrix.set(matrix);
+              start.set(event.getX(), event.getY());
+              Log.d(TAG, "mode=DRAG" );
+              mode = DRAG;
+              break;
+           case MotionEvent.ACTION_UP:
+           case MotionEvent.ACTION_POINTER_UP:
+              mode = NONE;
+              Log.d(TAG, "mode=NONE" );
+              break;
+           case MotionEvent.ACTION_POINTER_DOWN:
+         	   oldDist = spacing(event);
+         	   Log.d(TAG, "oldDist=" + oldDist);
+         	   if (oldDist > 10f) {
+         	      savedMatrix.set(matrix);
+         	      midPoint(mid, event);
+         	      mode = ZOOM;
+         	      Log.d(TAG, "mode=ZOOM" );
+         	   }
+         	   break;
+
+         	case MotionEvent.ACTION_MOVE:
+         	   if (mode == DRAG) {
+         		   matrix.set(savedMatrix);
+                    matrix.postTranslate(event.getX() - start.x,
+                    event.getY() - start.y);
+         	   }
+         	   else if (mode == ZOOM) {
+         	      float newDist = spacing(event);
+         	      Log.d(TAG, "newDist=" + newDist);
+         	      if (newDist > 10f) {
+         	         matrix.set(savedMatrix);
+         	         float scale = newDist / oldDist;
+         	         matrix.postScale(scale, scale, mid.x, mid.y);
+         	      }
+         	   }
+         	   break;
+        }
+
+           // Perform the transformation
+           view.setImageMatrix(matrix);
+
+           return true; // indicate event was handled
+        }
+        
+        
+        private void midPoint(PointF point, MotionEvent event) {
+     	   float x = event.getX(0) + event.getX(1);
+     	   float y = event.getY(0) + event.getY(1);
+     	   point.set(x / 2, y / 2);
+     	}
+
+        private float spacing(MotionEvent event) {
+     	   float x = event.getX(0) - event.getX(1);
+     	   float y = event.getY(0) - event.getY(1);
+     	   return FloatMath.sqrt(x * x + y * y);
+     	}
+
+     /** Show an event in the LogCat view, for debugging */
+        private void dumpEvent(MotionEvent event) {
+           String names[] = { "DOWN" , "UP" , "MOVE" , "CANCEL" , "OUTSIDE" ,
+              "POINTER_DOWN" , "POINTER_UP" , "7?" , "8?" , "9?" };
+           StringBuilder sb = new StringBuilder();
+           int action = event.getAction();
+           int actionCode = action & MotionEvent.ACTION_MASK;
+           sb.append("event ACTION_" ).append(names[actionCode]);
+           if (actionCode == MotionEvent.ACTION_POINTER_DOWN
+                 || actionCode == MotionEvent.ACTION_POINTER_UP) {
+              sb.append("(pid " ).append(
+              action >> MotionEvent.ACTION_POINTER_ID_SHIFT);
+              sb.append(")" );
+           }
+           sb.append("[" );
+           for (int i = 0; i < event.getPointerCount(); i++) {
+              sb.append("#" ).append(i);
+              sb.append("(pid " ).append(event.getPointerId(i));
+              sb.append(")=" ).append((int) event.getX(i));
+              sb.append("," ).append((int) event.getY(i));
+              if (i + 1 < event.getPointerCount())
+                 sb.append(";" );
+           }
+           sb.append("]" );
+           Log.d(TAG, sb.toString());
+        }
+        class CoolLayout extends LinearLayout
+        {
+        	private ImageView img;
+			public CoolLayout(Context context) {
+				super(context);
+				
+			}
+			public ImageView getImageView()
+			{
+				return img;
+			}
+			public void  setImageView(ImageView img)
+			{
+				this.img = img;
+				addView(this.img);
+			}
+			
+        	
+        }
         public GraphView(Context context) {
             super(context);
 
@@ -460,7 +601,7 @@ public class PdfViewerICE2Activity extends Activity {
 			LinearLayout.LayoutParams lpWrap10 = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT,10);
 
             // vertical layout
-			LinearLayout vl=new LinearLayout(context);
+			CoolLayout vl=new CoolLayout(context);
 			vl.setLayoutParams(lpWrap10);
 			vl.setOrientation(LinearLayout.VERTICAL);
 
@@ -502,8 +643,9 @@ public class PdfViewerICE2Activity extends Activity {
 		       // updateImage();
 		        mImageView.setLayoutParams(lpWrap1);
 		        mImageView.setPadding(5, 5, 5, 5);
-		        vl.addView(mImageView);	
-			
+		        mImageView.setScaleType(ScaleType.MATRIX);
+		        vl.setImageView(mImageView);	
+		       // mImageView.setOnTouchListener(this);
 		        //addNavButtons(vl);
 			    
 			setLayoutParams(new LayoutParams(
@@ -515,7 +657,9 @@ public class PdfViewerICE2Activity extends Activity {
 			setHorizontalFadingEdgeEnabled(true);
 			setVerticalScrollBarEnabled(true);
 			setVerticalFadingEdgeEnabled(true);
+			vl.setOnTouchListener(this);
 			addView(vl);
+			
         }
 
         private void addNavButtons(ViewGroup vg) {
@@ -877,6 +1021,14 @@ public class PdfViewerICE2Activity extends Activity {
     		mTmpFile = null;
     	}
     }
+    
+    
+    
+    
+    
+   // private static final String TAG = "Touch" ;
+ // These matrices will be used to move and zoom image
+
     
 
 }
